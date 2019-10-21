d4b88485bb9140c480e52a4ca9a794c56ef3655c
Ivan Melnikov <ivanempire@gmail.com>
2019-10-21 10:45:47 -0400

    Dependencies and icon

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="https://www.linkedin.com/in/imelnikov" uk-icon="icon: linkedin; ratio: 2"></a></li>
 		<li><a href="https://www.500px.com/ivanempire" uk-icon="icon: 500px; ratio: 2"></a></li>
-		<li><a :href="resumeLink" uk-icon="icon: 500px; ratio: 2"></a></li>
+		<li><a :href="resumeLink" uk-icon="icon: file-pdf; ratio: 2"></a></li>
 	</ul>
 </template>
 
