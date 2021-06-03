d202ff25face05b75f97d1d74e717c8ee3b1922d
Ivan Melnikov <ivanempire@gmail.com>
2021-06-03 15:42:29 -0400

    Removing public path prefix

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
     mode: "development",
     output: {
         filename: "notivan.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/"
+        path: path.resolve(__dirname, "dist")
     },
     devServer: {
         publicPath: "/",
